794bd0f1ce90400cfc3cef4819676a2239d77f38
vipe <vincenzo.petronio@outlook.com>
2015-01-12 17:07:58 +0100

    Windows Phone porting

--- ImgurViral/ImgurViral.WindowsPhone/Views/AuthView.xaml.cs
+++ ImgurViral/ImgurViral.WindowsPhone/Views/AuthView.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ImgurViral.Models;
+using ImgurViral.Utils;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -36,14 +39,42 @@ namespace ImgurViral.Views
         {
         }
 
-        private void WebView_Loaded(object sender, RoutedEventArgs e)
+        private async void WebView_Loaded(object sender, RoutedEventArgs e)
         {
-
+            AuthUser authUser = null;
+            try
+            {
+                Debug.WriteLine("[AuthView.WebView_Loaded]\t" + "Try to load local token");
+                authUser = await AuthHelper.ReadAuthData();
+                Frame.Navigate(typeof(MainPageView));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("[AuthView.WebView_Loaded]\t" + "Catch exception, local token not found. Move to Imgur login page!");
+                Debug.WriteLine("[AuthView.WebView_Loaded]\n" + ex.Message);
+                // Request: https://api.imgur.com/oauth2/authorize?client_id=CLIENT_ID&response_type=token
+                this.webView.Navigate(new Uri(String.Format(Constants.ENDPOINT_API_AUTHORIZE, Constants.API_CLIENTID)));
+            }
         }
 
-        private void webView_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+        private async void webView_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
         {
+            // Response: http://example.com#access_token=ACCESS_TOKEN&token_type=Bearer&expires_in=3600
+            string uriToString = args.Uri.ToString();
+            Debug.WriteLine("[AuthView.webView_NavigationStarting]\t" + uriToString);
+            if (uriToString.Contains(Constants.AUTH_ACCESS_TOKEN))
+            {
+                AuthUser authUser = await AuthHelper.CreateAuthUser(uriToString, true);
 
+                if (await AuthHelper.SaveAuthData(authUser))
+                {
+                    Frame.Navigate(typeof(MainPageView));
+                }
+            }
+            else
+            {
+                Debug.WriteLine("[AuthView.webView_NavigationStarting]\t" + "NO ACCESS TOKEN FROM URI!");
+            }
         }
     }
 }
