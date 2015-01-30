5c8c8c5bc231f71ca55a7061423465bd04c4657f
vipe <vincenzo.petronio@outlook.com>
2015-01-30 12:53:08 +0100

    ADD minimal layout for Windows project

--- ImgurViral/ImgurViral.Windows/Views/MainPageView.xaml.cs
+++ ImgurViral/ImgurViral.Windows/Views/MainPageView.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +27,40 @@ namespace ImgurViral.Views
         public MainPageView()
         {
             this.InitializeComponent();
+
+            this.Loaded += page_Loaded;
+        }
+
+        private void page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Window.Current.SizeChanged += Window_SizeChanged;
+        }
+
+        private void Window_SizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+        {
+            var visualState = "DefaultLayout";
+            var applicationView = ApplicationView.GetForCurrentView();
+            if (applicationView.IsFullScreen)
+            {
+                //if (applicationView.Orientation == ApplicationViewOrientation.Landscape)
+                //    visualState = "FullScreenLandscape";
+                //else
+                //    visualState = "FullScreenPortrait";
+            }
+            else
+            {
+                var size = Window.Current.Bounds;
+                if (size.Width == 320) {
+                    //visualState = "SnappedLayout";
+                }
+                else if (size.Width <= 500) {
+                    visualState = "MinimalLayout";
+                }
+                //else
+                //    visualState = "FilledLayout";
+            }
+            
+            VisualStateManager.GoToState(this, visualState, true);
         }
     }
 }
